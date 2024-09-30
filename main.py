06a7a6fed727836f8fe7f89ac575547e041ed196
Thisalini <thisalini.ravinthiran@edu.esiee.fr>
2024-09-30 12:28:01 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-#### Fonction secondaire
-
-
+####Fonction secondaire
+""" écrire un programme permettant de voir si un mot est -il un palindromes"""
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """vérifier si p est un palindromes
+    arg:p"""
+    p1 = p.lower()
+    mytable = str.maketrans("éèêëàâôç-'.;:!?,","eeeeaaoc        ")
+    p2= p1.translate(mytable)
+    p3 = p2.replace(" ","")
+    if p3[::-1] == p3[::1]:
+        return True
     return False
 
+
 #### Fonction principale
-
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """ Dire si P est un palindrome"""
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
